57969b12088a349c83090995a8c37cef977e0250
Madusudanan.B.N <b.n.madusudanan@gmail.com>
2013-04-01 13:12:14 +0530

    Update GetXPATH_init.js

--- GetXPATH_init.js
+++ GetXPATH_init.js
@@ -1,1 +1,43 @@
-javascript:function getNode(node){var nodeExpr=node.tagName;if(!nodeExpr)return null;if(node.id!=''){nodeExpr+="[@id='"+node.id+"']";return "/"+nodeExpr;}var rank=1;var ps=node.previousSibling;while(ps){if(ps.tagName==node.tagName){rank++;}ps=ps.previousSibling;}if(rank>1){nodeExpr+='['+rank+']';}else{var ns=node.nextSibling;while(ns){if(ns.tagName==node.tagName){nodeExpr+='[1]';break;}ns=ns.nextSibling;}}return nodeExpr;}
+/* Save this file content as a bookmark in Internet Explorer
+This script helps to get the XPath of an element in IE-similar to Firefox's firebug/firepath
+Select any element in a webpage and click on the two bookmarks GetXPATH_init.js,GetXPATH_final.js in the mentioned order to get the XPath of that element.
+*/
+
+javascript:function getNode(node)
+{
+var nodeExpr=node.tagName;
+if(!nodeExpr)return null;
+if(node.id!='')
+{
+nodeExpr+="[@id='"+node.id+"']";
+return "/"+nodeExpr;
+}
+var rank=1;
+var ps=node.previousSibling;
+while(ps)
+{
+if(ps.tagName==node.tagName)
+{
+rank++;
+}
+ps=ps.previousSibling;
+}
+if(rank>1)
+{
+nodeExpr+='['+rank+']';
+}
+else
+{
+var ns=node.nextSibling;
+while(ns)
+{
+if(ns.tagName==node.tagName)
+{
+nodeExpr+='[1]';
+break;
+}
+ns=ns.nextSibling;
+}
+}
+return nodeExpr;
+}
