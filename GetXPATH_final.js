830d64ca30d3daca774b6ddc2e3f0568b8aac304
Madusudanan.B.N <b.n.madusudanan@gmail.com>
2013-04-01 13:15:18 +0530

    Update GetXPATH_final.js

--- GetXPATH_final.js
+++ GetXPATH_final.js
@@ -1,3 +1,18 @@
-javascript:function o__o()
+javascript:function GetXPATH_final()
 {
-var currentNode=document.selection.createRange().parentElement();var path=[];while(currentNode){var pe=getNode(currentNode);if(pe){path.push(pe);if(pe.indexOf('@id')!=-1)break;}currentNode=currentNode.parentNode;}var xpath="/"+path.reverse().join('/');clipboardData.setData("Text", xpath);}o__o();
+var currentNode=document.selection.createRange().parentElement();
+var path=[];
+while(currentNode)
+{
+var pe=getNode(currentNode);
+if(pe)
+{
+path.push(pe);
+if(pe.indexOf('@id')!=-1)break;
+}
+currentNode=currentNode.parentNode;
+}
+var xpath="/"+path.reverse().join('/');
+clipboardData.setData("Text", xpath);
+}
+GetXPATH_final();
