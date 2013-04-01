f31fa004af6a4d9cce814ffc5d794d7fb9d08e02
Madusudanan.B.N <b.n.madusudanan@gmail.com>
2013-04-01 13:16:27 +0530

    Update GetXPATH_final.js

--- GetXPATH_final.js
+++ GetXPATH_final.js
@@ -1,3 +1,8 @@
+/* Save this file content as a bookmark in Internet Explorer
+This script helps to get the XPath of an element in IE-similar to Firefox's firebug/firepath
+Select any element in a webpage and click on the two bookmarks GetXPATH_init.js,GetXPATH_final.js in the respective order to get the XPath of that element.
+*/
+
 javascript:function GetXPATH_final()
 {
 var currentNode=document.selection.createRange().parentElement();
